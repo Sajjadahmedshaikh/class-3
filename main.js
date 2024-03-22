c9599093468188245a5e197aba50fa39d7878e8f
Sajjadahmedshaikh <160344749+Sajjadahmedshaikh@users.noreply.github.com>
2024-03-21 18:26:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var fname = "ali";
+console.log(fname);
+var score = 25;
+console.log(score);
+var isnew = true;
+console.log(isnew);
+var myname = "daniyal";
+var hello = "hello" + myname;
+console.log(hello); //hellodaniyal
+var safa = "safa";
+var shafaq = "shafaq";
+console.log("".concat(safa, " ").concat(shafaq));
---
+++ main.ts
@@ -0,0 +1,21 @@
+let fname="ali";
+
+console.log(fname)
+
+let score=25;
+
+console.log(score)
+
+let isnew=true;
+
+console.log(isnew)
+
+var myname="daniyal"
+var hello="hello"+myname;
+
+console.log(hello);//hellodaniyal
+
+let safa="safa"
+let shafaq="shafaq"
+
+console.log(`${safa} ${shafaq}`)
